4fc989f05603d41a502139bdaec4ce169308d635
austiniqer <63011075@kmitl.ac.th>
2023-08-17 09:09:39 +0700

    latest

--- Synchronization/case-study/Program.cs
+++ Synchronization/case-study/Program.cs
@@ -9,6 +9,9 @@ namespace OS_Problem_02
         static int Front = 0;
         static int Back = 0;
         static int Count = 0;
+        static object locker = new object();
+        static int EnCount = 0;
+        static int DeCount = 0;
 
         static void EnQueue(int eq)
         {
@@ -16,6 +19,8 @@ namespace OS_Problem_02
             Back++;
             Back %= 10;
             Count += 1;
+            EnCount++;
+            // Console.WriteLine("+ EnQ, Queue now = {0}, EnQ total = {1}", Count, EnCount);
         }
 
         static int DeQueue()
@@ -25,28 +30,48 @@ namespace OS_Problem_02
             Front++;
             Front %= 10;
             Count -= 1;
+            DeCount++;
+            // Console.WriteLine("- DeQ, Queue now = {0}, DeQ total = {1}", Count, DeCount);
             return x;
         }
 
         static void th01()
         {
             int i;
-
             for (i = 1; i < 51; i++)
             {
-                EnQueue(i);
-                Thread.Sleep(5);
+                lock (locker)
+                {
+                    if (Count < 10)
+                    {
+                        EnQueue(i);
+                    }
+                    else
+                    {
+                        i--;
+                        // continue;
+                    }
+                }
             }
         }
 
         static void th011()
         {
             int i;
-
             for (i = 100; i < 151; i++)
             {
-                EnQueue(i);
-                Thread.Sleep(5);
+                lock (locker)
+                {
+                    if (Count < 10)
+                    {
+                        EnQueue(i);
+                    }
+                    else
+                    {
+                        i--;
+                        // continue;
+                    }
+                }
             }
         }
 
@@ -55,27 +80,37 @@ namespace OS_Problem_02
         {
             int i;
             int j;
-
-            for (i=0; i< 60; i++)
+            for (i = 0; i < 60; i++)
             {
-                j = DeQueue();
-                Console.WriteLine("j={0}, thread:{1}", j, t);
-                Thread.Sleep(100);
+                lock (locker)
+                {
+                    if (DeCount == 101) break;
+                    if (Count > 0)
+                    {
+                        j = DeQueue();
+                        Console.WriteLine("j={0} | thread:{1} | count = {2}", j, t, Count);
+                    }
+                    else
+                    {
+                        i--;
+                        // continue;
+                    }
+                }
             }
         }
         static void Main(string[] args)
         {
             Thread t1 = new Thread(th01);
-            //Thread t11 = new Thread(th011);
+            Thread t11 = new Thread(th011);
             Thread t2 = new Thread(th02);
-            //Thread t21 = new Thread(th02);
-            //Thread t22 = new Thread(th02);
+            Thread t21 = new Thread(th02);
+            Thread t22 = new Thread(th02);
 
             t1.Start();
-            //t11.Start();
+            t11.Start();
             t2.Start(1);
-            //t21.Start(2);
-            //t22.Start(3);
+            t21.Start(2);
+            t22.Start(3);
         }
     }
 }
--- Synchronization/ex05/Program.cs
+++ Synchronization/ex05/Program.cs
@@ -1,80 +1,116 @@
 ﻿using System;
 using System.Threading;
 
-namespace cv_lab
+namespace OS_Problem_02
 {
-	class Program 
-	{
-		private static string x = "";
-		private static int exitflag = 0;
-		private static bool updateFlag = false;
-		
-		private static bool entered = false;
+    class Thread_safe_buffer
+    {
+        static int[] TSBuffer = new int[10];
+        static int Front = 0;
+        static int Back = 0;
+        static int Count = 0;
+        static object locker = new object();
+        static int EnCount = 0;
+        static int DeCount = 0;
 
-		private static Object _Lock = new Object();
+        static void EnQueue(int eq)
+        {
+            TSBuffer[Back] = eq;
+            Back++;
+            Back %= 10;
+            Count += 1;
+            EnCount++;
+            Console.WriteLine("+ EnQ, Queue now = {0}, EnQ total = {1}", Count, EnCount);
+        }
 
-		// private static Semaphore _smp = new Semaphore(1,1);
-		
+        static int DeQueue()
+        {
+            int x = 0;
+            x = TSBuffer[Front];
+            Front++;
+            Front %= 10;
+            Count -= 1;
+            DeCount++;
+            Console.WriteLine("- DeQ, Queue now = {0}, DeQ total = {1}", Count, DeCount);
+            return x;
+        }
 
-		static void ThReadX(object i)
-		{
-			while( exitflag == 0)
-			{
-				try
-				{
-					Monitor.Enter(_Lock);
-					if (!entered) throw new Exception();
-					if ( updateFlag == false) throw new Exception();
-					if ( x != "exit")
-					{
-						System.Console.WriteLine($"***Thread {i} : x = {x}***)");
-						updateFlag = false;
-					}
-					Thread.Sleep(105);
-				}
-				catch (System.Exception)
-				{
-					// throw;
-				}
-				finally{
-					Monitor.Exit(_Lock);
-				}
-			}
-			System.Console.WriteLine($"---Thread {i} exit---");
-		}
+        static void th01()
+        {
+            int i;
+            for (i = 1; i < 51; i++)
+            {
+                lock (locker)
+                {
+                    if (Count < 10)
+                    {
+                        EnQueue(i);
+                    }
+                    else
+                    {
+                        i--;
+                        continue;
+                    }
+                }
+            }
+        }
 
-		static void ThWriteX()
-		{
-			string xx;
-			while( exitflag == 0){
-				// lock (_Lock)
-				// {
-					Monitor.Enter(_Lock);
-					System.Console.Write("Input: ");
-					xx = System.Console.ReadLine();
-					if ( xx == "exit")
-						exitflag = 1;
-					x = xx;
-					updateFlag = true;
-					entered = true;
-					// Thread.Sleep(500);
-				// }
-					Monitor.Exit(_Lock);
-			}
-		}
+        static void th011()
+        {
+            int i;
+            for (i = 100; i < 151; i++)
+            {
+                lock (locker)
+                {
+                    if (Count < 10)
+                    {
+                        EnQueue(i);
+                    }
+                    else
+                    {
+                        i--;
+                        continue;
+                    }
+                }
+            }
+        }
 
-		static void Main(string[] args)
-		{
-			Thread A = new Thread(ThWriteX);
-			Thread B = new Thread(ThReadX);
-			Thread C = new Thread(ThReadX);
-			Thread D = new Thread(ThReadX);
 
-			A.Start();
-			B.Start(1);
-			C.Start(2);
-			D.Start(3);
+        static void th02(object t)
+        {
+            int i;
+            int j;
+            for (i = 0; i < 60; i++)
+            {
+                lock (locker)
+                {
+                    if (DeCount == 101) break;
+                    if (Count > 0)
+                    {
+                        j = DeQueue();
+                        // Console.WriteLine("j={0}, thread:{1} count = {2}", j, t, Count);
+                    }
+                    else
+                    {
+                        i--;
+                        continue;
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(th01);
+            Thread t11 = new Thread(th011);
+            Thread t2 = new Thread(th02);
+            Thread t21 = new Thread(th02);
+            Thread t22 = new Thread(th02);
 
-		}
-	}
+            t1.Start();
+            t11.Start();
+            t2.Start(1);
+            t21.Start(2);
+            t22.Start(3);
+        }
+    }
 }
